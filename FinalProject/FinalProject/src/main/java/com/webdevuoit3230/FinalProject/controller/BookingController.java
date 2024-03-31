2798016b60812c99bc274508d8ac27d3ca883cc6
AndresCardoso <andres.cardoso@ontariotechu.net>
2024-03-31 16:14:28 -0400

    correct the file location for style.css file

--- FinalProject/FinalProject/src/main/java/com/webdevuoit3230/FinalProject/controller/BookingController.java
+++ FinalProject/FinalProject/src/main/java/com/webdevuoit3230/FinalProject/controller/BookingController.java
@@ -1,5 +1,16 @@
 package com.webdevuoit3230.FinalProject.controller;
 
+
+import jakarta.servlet.http.HttpSession;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import com.webdevuoit3230.FinalProject.model.Booking;
+import java.util.ArrayList;
+import java.util.List;
+
 public class BookingController {
 
 }
