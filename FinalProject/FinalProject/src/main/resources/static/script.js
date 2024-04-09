646cf4afd9fb05a08c6bc1b31d2d9b9480a80cac
AndresCardoso <andres.cardoso@ontariotechu.net>
2024-04-09 01:08:46 -0400

    edited the select tool in the room service page

--- FinalProject/FinalProject/src/main/resources/static/script.js
+++ FinalProject/FinalProject/src/main/resources/static/script.js
@@ -8,3 +8,36 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     });
 });
+
+document.addEventListener('DOMContentLoaded', function () {
+    var typeInput = document.getElementById('type');
+    var priceInput = document.getElementById('price');
+
+    if (typeInput.value === 'Single') {
+        priceInput.value = 120;
+    } else if (typeInput.value === 'Double') {
+        priceInput.value = 180;
+    }
+
+    typeInput.addEventListener('change', function () {
+        if (this.value === 'Single') {
+            priceInput.value = 120;
+        } else if (this.value === 'Double') {
+            priceInput.value = 180;
+        }
+    });
+});
+
+
+function highlight(img, service) {
+    // Toggle the highlighted class
+    img.classList.toggle('highlighted');
+    
+    // Update the value of the hidden input based on selected images
+    var selectedServices = [];
+    var highlightedImages = document.querySelectorAll('.listing img.highlighted');
+    highlightedImages.forEach(function(image) {
+        selectedServices.push(image.getAttribute('alt'));
+    });
+    document.getElementById('selectedService').value = selectedServices.join(', ');
+}
